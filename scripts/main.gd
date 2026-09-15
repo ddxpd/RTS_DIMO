4200fd78fac4ce736d77e08d2fb4bf2678addee7
ddxpd <ddxpd@users.noreply.github.com>
2026-09-16 00:00:04 +0800

    Fix mixed indentation

--- scripts/main.gd
+++ scripts/main.gd
@@ -730,13 +730,13 @@ func _refresh_ui() -> void:
 		action_buttons[0].text = "STOP\n[S]"
 		action_buttons[0].disabled = false
 		action_buttons[1].text = "MOVE\nRight click"
-		 action_buttons[1].disabled = false
+		action_buttons[1].disabled = false
 		if selected.type == "soldier":
 			action_buttons[2].text = "ATTACK\n[A]"
-		 action_buttons[2].disabled = false
+		action_buttons[2].disabled = false
 		else:
 			action_buttons[2].text = "GATHER\nRight click ore"
-		 action_buttons[2].disabled = false
+		action_buttons[2].disabled = false
 	resume_button.disabled = not active
 	message_label.text = feedback if feedback_time > 0 else "Left: select    Right: order    A: attack mode    B: barracks    S: stop    Esc: menu"
 	result_label.text = ""
@@ -866,6 +866,7 @@ func _draw() -> void:
 func _bar(pos: Vector2, width: float, fraction: float, color: Color) -> void:
 	draw_rect(Rect2(pos, Vector2(width, 4)), Color("#182219"))
 	draw_rect(Rect2(pos, Vector2(width * clampf(fraction, 0, 1), 4)), color)
+
 
 
 
